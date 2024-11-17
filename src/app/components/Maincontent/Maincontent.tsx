41ed3eddbcf6363cb88b4beb40b900416ee703ed
@4drtoth <adrian.toth@graniteme.com>
2024-11-17 21:06:58 +0000

    merging sidebar v1

--- src/app/components/Maincontent/Maincontent.tsx
+++ src/app/components/Maincontent/Maincontent.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from 'react';
-import Map from '../Map';
+import Map from '../map';
 import Factions from '../Factions';
 
 interface MaincontentProps {
