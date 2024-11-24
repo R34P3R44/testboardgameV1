b57c3e9b959698465e786f2f725a228bdc96835f
@4drtoth <adrian.toth@graniteme.com>
2024-11-24 21:43:36 +0000

    fixed import for Map.tsx

--- src/app/components/Maincontent/Maincontent.tsx
+++ src/app/components/Maincontent/Maincontent.tsx
@@ -4,21 +4,21 @@ import Map from '../Map';
 import Factions from '../Factions';
 
 interface MaincontentProps {
-    activeItem: string | null;
-    onCloseModal: () => void;
-    isActiveModal: boolean;
+  activeItem: string | null;
+  onCloseModal: () => void;
+  isActiveModal: boolean;
 }
 
-const Maincontent: React.FC<MaincontentProps> = ({ activeItem, onCloseModal, isActiveModal}) => { 
+const Maincontent: React.FC<MaincontentProps> = ({ activeItem, onCloseModal, isActiveModal }) => {
 
   return (
     <div>
-        <Map/>
-        {isActiveModal && activeItem === 'Factions' ? 
-            <Factions onCloseModal={onCloseModal} activeItem={activeItem}/>
-            :
-            null
-        }
+      <Map />
+      {isActiveModal && activeItem === 'Factions' ?
+        <Factions onCloseModal={onCloseModal} activeItem={activeItem} />
+        :
+        null
+      }
     </div>
   )
 };
